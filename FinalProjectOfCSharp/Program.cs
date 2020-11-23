ac42ab3bc5ba76759e80577414c759205c24ed5f
subhan-ezimli <subhanav@code.edu.az>
2020-11-23 20:54:26 +0400

    Ïnfrastructur is Created

---
+++ FinalProjectOfCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FinalProjectOfCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
